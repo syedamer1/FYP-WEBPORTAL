20b240ac715e73ff76f5d89362c5576a7eb02bb4
Syed Muhammad Amer Ali Zaidi <amir.ali2036a@gmail.com>
2024-05-18 18:06:28 +0500

    Added 404 Page Not Found

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,13 @@
-import { useRoutes } from 'react-router-dom';
-import MainRoutes from './MainRoutes';
-import LoginRoute from './LoginRoute';
+import { useRoutes } from "react-router-dom";
+import MainRoutes from "./MainRoutes";
+import LoginRoute from "./LoginRoute";
+import NotFoundPage from "../pages/extra/NotFoundPage";
 const Routes = () => {
-  return useRoutes([LoginRoute,MainRoutes]);
+  return useRoutes([
+    LoginRoute,
+    MainRoutes,
+    { path: "*", element: <NotFoundPage /> },
+  ]);
 };
 
 export default Routes;
