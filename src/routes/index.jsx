034ac9ac7440673ad4c44ff74c231bb0957ce94c
Syed Muhammad Amer Ali Zaidi <amir.ali2036a@gmail.com>
2024-05-18 18:29:25 +0500

    Fix 404NotFoundPage Deployment Error

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { useRoutes } from "react-router-dom";
 import MainRoutes from "./MainRoutes";
 import LoginRoute from "./LoginRoute";
+import { Navigate } from "react-router-dom";
 import NotFoundPage from "../pages/extra/NotFoundPage";
 const Routes = () => {
   return useRoutes([
     LoginRoute,
     MainRoutes,
-    { path: "*", element: <NotFoundPage /> },
+    { path: "*", element: <Navigate to="/404NotFound" /> },
+    { path: "/404NotFound", element: <NotFoundPage /> },
   ]);
 };
 
