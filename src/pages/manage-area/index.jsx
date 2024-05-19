ac0d99ad7960bcdf59fc8ba7c0e32cf9edeef714
Syed Muhammad Amer Ali Zaidi <amir.ali2036a@gmail.com>
2024-05-19 18:13:23 +0500

    Add tabs for better rendering

--- src/pages/manage-area/index.jsx
+++ src/pages/manage-area/index.jsx
@@ -1,12 +1,48 @@
+import { useState } from "react";
+import { Typography, Tabs, Tab, Box } from "@mui/material/";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CustomCard from "@components/CustomCard";
-import { Typography, Grid } from "@mui/material/";
 import ProvinceTable from "./province/ProvinceTable";
 import DivisionTable from "./division/DivisionTable";
 import DistrictTable from "./district/DistrictTable";
 import TehsilTable from "./tehsil/TehsilTable";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  LocationOnOutlined,
+  AccountTreeOutlined,
+  BusinessOutlined,
+  LocationCityOutlined,
+} from "@mui/icons-material";
+
+const a11yProps = (index) => {
+  return {
+    id: `tab-${index}`,
+    "aria-controls": `tabpanel-${index}`,
+  };
+};
+
+const TabPanel = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 2 }}>{children}</Box>}
+    </div>
+  );
+};
 
 const ManageArea = () => {
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const provinceQueryClient = new QueryClient();
   const divisionQueryClient = new QueryClient();
   const districtQueryClient = new QueryClient();
@@ -14,41 +50,75 @@ const ManageArea = () => {
 
   return (
     <>
-      <Grid item xs={12} sm={6} sx={{ mb: -2.25 }}>
-        <Typography variant="h5">Manage Area</Typography>
-      </Grid>
-      <CustomCard sx={{ mt: 5, mb: 5 }}>
-        <Typography variant="h3" sx={{ marginBottom: 2 }}>
-          Manage Province
-        </Typography>
-        <QueryClientProvider client={provinceQueryClient}>
-          <ProvinceTable />
-        </QueryClientProvider>
-      </CustomCard>
-      <CustomCard sx={{ mt: 5, mb: 5 }}>
-        <Typography variant="h3" sx={{ marginBottom: 2 }}>
-          Manage Division
-        </Typography>
-        <QueryClientProvider client={divisionQueryClient}>
-          <DivisionTable />
-        </QueryClientProvider>
-      </CustomCard>
-      <CustomCard sx={{ mt: 5, mb: 5 }}>
-        <Typography variant="h3" sx={{ marginBottom: 2 }}>
-          Manage District
-        </Typography>
-        <QueryClientProvider client={districtQueryClient}>
-          <DistrictTable />
-        </QueryClientProvider>
-      </CustomCard>
-      <CustomCard sx={{ mt: 5, mb: 5 }}>
-        <Typography variant="h3" sx={{ marginBottom: 2 }}>
-          Manage Tehsil
-        </Typography>
-        <QueryClientProvider client={tehsilQueryClient}>
-          <TehsilTable />
-        </QueryClientProvider>
-      </CustomCard>
+      <Typography variant="h5" sx={{ mb: 2 }}>
+        Manage Area
+      </Typography>
+      <Tabs value={value} onChange={handleChange} aria-label="manage area tabs">
+        <Tab
+          label="Province"
+          icon={<LocationOnOutlined />}
+          iconPosition="start"
+          {...a11yProps(0)}
+        />
+        <Tab
+          label="Division"
+          icon={<AccountTreeOutlined />}
+          iconPosition="start"
+          {...a11yProps(1)}
+        />
+        <Tab
+          label="District"
+          icon={<BusinessOutlined />}
+          iconPosition="start"
+          {...a11yProps(2)}
+        />
+        <Tab
+          label="Tehsil"
+          icon={<LocationCityOutlined />}
+          iconPosition="start"
+          {...a11yProps(3)}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <CustomCard sx={{ mt: 5, mb: 5 }}>
+          <Typography variant="h3" sx={{ marginBottom: 2 }}>
+            Manage Province
+          </Typography>
+          <QueryClientProvider client={provinceQueryClient}>
+            <ProvinceTable />
+          </QueryClientProvider>
+        </CustomCard>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <CustomCard sx={{ mt: 5, mb: 5 }}>
+          <Typography variant="h3" sx={{ marginBottom: 2 }}>
+            Manage Division
+          </Typography>
+          <QueryClientProvider client={divisionQueryClient}>
+            <DivisionTable />
+          </QueryClientProvider>
+        </CustomCard>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <CustomCard sx={{ mt: 5, mb: 5 }}>
+          <Typography variant="h3" sx={{ marginBottom: 2 }}>
+            Manage District
+          </Typography>
+          <QueryClientProvider client={districtQueryClient}>
+            <DistrictTable />
+          </QueryClientProvider>
+        </CustomCard>
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <CustomCard sx={{ mt: 5, mb: 5 }}>
+          <Typography variant="h3" sx={{ marginBottom: 2 }}>
+            Manage Tehsil
+          </Typography>
+          <QueryClientProvider client={tehsilQueryClient}>
+            <TehsilTable />
+          </QueryClientProvider>
+        </CustomCard>
+      </TabPanel>
     </>
   );
 };
